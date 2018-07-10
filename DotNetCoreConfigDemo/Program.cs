80bcd1e7954b1160d8c5a499428c31cfff7068e0
madhub <b.madhu@outlook.com>
2018-07-10 17:15:42 +0530

    updated with POCO bind sample

--- DotNetCoreConfigDemo/Program.cs
+++ DotNetCoreConfigDemo/Program.cs
@@ -1,9 +1,39 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 
 namespace DotNetCoreConfigDemo
 {
+    public class Connection
+    {
+        public string serviceName { get; set; }
+        public string hostName { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        
+    }
+
+    public class ExchangeDetail
+    {
+        public string name { get; set; }
+        public string type { get; set; }
+    }
+
+    public class QueueDetail
+    {
+        public string name { get; set; }
+        public string exchangeName { get; set; }
+    }
+
+    public class EventBusConfiguration
+    {
+        public string eventBusType { get; set; }
+        public int RetryCount { get; set; }
+        public Connection connection { get; set; }
+        public List<ExchangeDetail> exchangeDetails { get; set; }
+        public List<QueueDetail> queueDetails { get; set; }
+    }
     /// <summary>
     /// sample implementation of custom configuration provider
     /// 
@@ -50,6 +80,10 @@ namespace DotNetCoreConfigDemo
     {
         static void Main(string[] args)
         {
+            //setup our DI
+            var serviceProvider = new ServiceCollection();
+            
+           
             // Env is a reference to the IHostingEnvironment instance
             // that you might want to inject in the class via ctor
             var dom = new ConfigurationBuilder()
@@ -70,7 +104,14 @@ namespace DotNetCoreConfigDemo
             //          configured in appconfig.json & environment variable 
             //          Example: dotnet DotNetCoreConfigDemo.dll eventBusConfiguration:connection:hostname=cmdhost
 
+            
             Console.WriteLine(dom.GetSection("eventBusConfiguration:connection")["hostname"]);
+            var config = new EventBusConfiguration();
+            dom.Bind("eventBusConfiguration", config);      //  <--- This
+            //dom.Bind()
+            //var section = serviceProvider.Configure<EventBusConfiguration>(dom.GetSection("eventBusConfiguration"));
+            //serviceProvider
+            ////IOptions<PagingOptions> config
             Console.ReadLine();
         }
        
