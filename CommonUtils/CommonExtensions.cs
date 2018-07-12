23b55946b07ed0a945b5143efed32faf816426d2
madhub <b.madhu@outlook.com>
2018-07-13 00:28:49 +0530

    .

---
+++ CommonUtils/CommonExtensions.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonUtils
+{
+    public static class CommonExtensions
+    {
+        // https://www.danylkoweb.com/Blog/10-extremely-useful-net-extension-methods-8J
+        public static string ToFileSize(this long size)
+        {
+            if (size < 1024) { return (size).ToString("F0") + " bytes"; }
+            if (size < Math.Pow(1024, 2)) { return (size / 1024).ToString("F0") + "KB"; }
+            if (size < Math.Pow(1024, 3)) { return (size / Math.Pow(1024, 2)).ToString("F0") + "MB"; }
+            if (size < Math.Pow(1024, 4)) { return (size / Math.Pow(1024, 3)).ToString("F0") + "GB"; }
+            if (size < Math.Pow(1024, 5)) { return (size / Math.Pow(1024, 4)).ToString("F0") + "TB"; }
+            if (size < Math.Pow(1024, 6)) { return (size / Math.Pow(1024, 5)).ToString("F0") + "PB"; }
+            return (size / Math.Pow(1024, 6)).ToString("F0") + "EB";
+        }
+        public static bool Between(this DateTime dt, DateTime rangeBeg, DateTime rangeEnd)
+        {
+            return dt.Ticks >= rangeBeg.Ticks && dt.Ticks <= rangeEnd.Ticks;
+        }
+        public static int CalculateAge(this DateTime dateTime)
+        {
+            var age = DateTime.Now.Year - dateTime.Year;
+            if (DateTime.Now < dateTime.AddYears(age))
+                age--;
+            return age;
+        }
+        /// <summary>
+        /// Usage
+        /// public Test(string input1)
+        /// {
+        ///     input1.ThrowIfArgumentIsNull(nameof(input1));
+        /// }
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="parameterName"></param>
+        public static void ThrowIfArgumentIsNull<T>(this T obj, string parameterName) where T : class
+        {
+            if (obj == null) throw new ArgumentNullException(parameterName + " not allowed to be null");
+        }
+        public static void ThrowIfNull<T>(this T item, string name) where T : class
+        { if (item == null) throw new ArgumentNullException(name); }
+
+        public static void ThrowIfNull<T>(this T item) where T : class
+        { if (item == null) throw new ArgumentNullException(); }
+
+        public static void ThrowIfNull<T>(this T? item, string name) where T : struct
+        { if (item == null) throw new ArgumentNullException(name); }
+
+        public static void ThrowIfNull<T>(this T? item) where T : struct
+        { if (item == null) throw new ArgumentNullException(); }
+
+        // MyList.EmptyIfNull().Where(....)
+        public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T> pSeq)
+        {
+            return pSeq ?? Enumerable.Empty<T>();
+        }
+        public static bool IsNullOrEmpty(this ICollection obj)
+        {
+            return (obj == null || obj.Count == 0);
+        }
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> collection)
+        {
+            return (collection == null || !collection.Any() );
+        }
+)
+
+    }
+
+    public static class FrameworkExtensions
+    {
+        // a map function
+        public static void ForEach<T>(this IEnumerable<T> @enum, Action<T> mapFunction)
+        {
+            foreach (var item in @enum) mapFunction(item);
+        }
+    }
+}
---
+++ NewCShap7xFeatures/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using static CommonUtils.CommonExtensions;
+
+namespace NewCShap7xFeatures
+{
+    public class Person
+    {
+        // Auto-Properties Initializers
+        public string FirstName { get; } = string.Empty;
+        public string LastName { get; } = string.Empty;
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        // Expression Bodied Functions and Properties
+        public override string ToString() => FirstName + LastName;
+        // Additionally, C# 6 allows us to filter the exceptions that should be 
+        // caught not only based on the exception's type
+        public void SomeDemo()
+        {
+            try
+            {
+                var file = File.OpenRead(null);
+            }
+            catch (Exception exp) when ( exp as ArgumentNullException == null)
+            {
+                Console.WriteLine("argument null");                
+            }
+            catch (Exception exp) when (exp as IOException != null)
+            {
+
+            }
+            catch (Exception exp) when (exp.InnerException as ArgumentNullException != null)
+            {
+
+            }
+
+
+        }
+        
+
+    }
+    class Program
+    {
+
+        // Auto-Properties Initializers
+        public ConsoleColor Background { get; } = ConsoleColor.Green;
+
+        static void Main1(string[] args)
+        {
+
+            //string s = (DateTime.Now.Day > 20 ? "Month end" : throw new Exception("Too early!"));
+
+            // Null-conditional Operator
+            string nullvalue = null;
+            string message = nullvalue?.ToUpper() ?? "HELLO World";
+            Console.WriteLine(message);
+
+            // Dictionary Initializers
+            var themesDictionary = new Dictionary<string, ConsoleColor>
+            {
+                ["blue"]=ConsoleColor.Blue,
+                ["green"]=ConsoleColor.Green
+            };
+        }
+        public static void Main(string[] args)
+        {
+            try
+            {
+                PrintName(null);
+            }
+            catch (Exception exp)
+            {
+
+                Console.WriteLine(exp);
+            }
+
+            Console.ReadKey();
+            
+        }
+
+        public static void PrintName(string name)
+        {
+            name.ThrowIfArgumentIsNull(nameof(name));
+        }
+    }
+}
