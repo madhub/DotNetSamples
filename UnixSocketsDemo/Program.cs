f61c5e540daf5e1a5b8ec960123a0a1ebd4c5331
madhub <b.madhu@outlook.com>
2018-08-22 23:11:28 +0530

    Added sample project for using unix domain sockets using .net core 2.1

---
+++ UnixSocketsDemo/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UnixSocketsDemo
+{
+    class UnixSocketsOnWindows
+    {
+        public static void Demo()
+        {
+            string path = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+
+            var task = Task.Run(() =>
+            {
+                StartServer(path);
+            });
+            // wait for server to start
+            Thread.Sleep(2000);
+
+
+            StartClient(path);
+            Console.ReadLine();
+        }
+        private static void StartClient(String path)
+        {
+
+            var endPoint = new UnixDomainSocketEndPoint(path);
+            try
+            {
+                using (var client = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified))
+                {
+                    client.Connect(endPoint);
+                    Console.WriteLine($"[Client] Connected to ... ..{path}");
+                    String str = String.Empty;
+                    var bytes = new byte[100];
+                    while (!str.Equals("exit", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        Console.WriteLine("[Client]Enter something: ");
+                        var line = Console.ReadLine();
+                        client.Send(Encoding.UTF8.GetBytes(line));
+                        Console.Write("[Client]From Server: ");
+
+                        int byteRecv = client.Receive(bytes);
+                        str = Encoding.UTF8.GetString(bytes, 0, byteRecv);
+                        Console.WriteLine(str);
+                    }
+                }
+            }
+            finally
+            {
+                try { File.Delete(path); }
+                catch { }
+            }
+        }
+
+        private static void StartServer(String path)
+        {
+            var endPoint = new UnixDomainSocketEndPoint(path);
+            try
+            {
+                using (var server = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified))
+                {
+                    server.Bind(endPoint);
+                    Console.WriteLine($"[Server] Listening ... ..{path}");
+                    server.Listen(1);
+                    using (Socket accepted = server.Accept())
+                    {
+                        Console.WriteLine("[Server]Connection Accepted ..." + accepted.RemoteEndPoint.ToString());
+                        var bytes = new byte[100];
+                        while (true)
+                        {
+                            int byteRecv = accepted.Receive(bytes);
+                            String str = Encoding.UTF8.GetString(bytes, 0, byteRecv);
+                            Console.WriteLine("[Server]Received " + str);
+                            accepted.Send(Encoding.UTF8.GetBytes(str.ToUpper()));
+                        }
+
+                    }
+                }
+            }
+            finally
+            {
+                try { File.Delete(path); }
+                catch { }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ UnixSocketsDemo/UnixSocketTcpEcho.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Buffers;
+using System.IO;
+using System.IO.Pipelines;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnixSocketsDemo
+{
+
+    //class UnixSocketTcpEcho
+    //{
+    //    static async Task Main(string[] args)
+    //    {
+    //        if (args.Length > 0)
+    //        {
+    //            string filePath = args[0];
+    //            Console.WriteLine($"Connecting unix socket @ {filePath}");
+    //            await ConnectToServer(filePath);
+    //        }
+    //        else
+    //        {
+    //            await StartServer();
+    //        }
+            
+    //    }
+
+    //    private static async Task ConnectToServer(string filePath)
+    //    {
+    //        var endPoint = new UnixDomainSocketEndPoint(filePath);
+    //        using (var client = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified))
+    //        {
+    //            await client.ConnectAsync(endPoint);
+                
+               
+    //        }
+                
+            
+
+
+    //    }
+
+    //    private static async Task StartServer()
+    //    {
+    //        string path = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+    //        var endPoint = new UnixDomainSocketEndPoint(path);
+
+    //        var listenSocket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified);// new Socket(SocketType.Stream, ProtocolType.Tcp);
+    //        //listenSocket.Bind(new IPEndPoint(IPAddress.Loopback, 8087));
+    //        listenSocket.Bind(endPoint);
+    //        Console.WriteLine($"Listening on unix socket @{path}");
+
+    //        listenSocket.Listen(120);
+
+    //        while (true)
+    //        {
+    //            var socket = await listenSocket.AcceptAsync();
+    //            _ = ProcessLinesAsync(socket);
+    //        }
+    //    }
+
+    //    private static async Task ProcessLinesAsync(Socket socket)
+    //    {
+    //        Console.WriteLine($"[{socket.RemoteEndPoint}]: connected");
+
+    //        var pipe = new Pipe();
+    //        Task writing = FillPipeAsync(socket, pipe.Writer);
+    //        Task reading = ReadPipeAsync(socket, pipe.Reader);
+
+    //        await Task.WhenAll(reading, writing);
+
+    //        Console.WriteLine($"[{socket.RemoteEndPoint}]: disconnected");
+    //    }
+
+    //    private static async Task FillPipeAsync(Socket socket, PipeWriter writer)
+    //    {
+    //        const int minimumBufferSize = 512;
+
+    //        while (true)
+    //        {
+    //            try
+    //            {
+    //                // Request a minimum of 512 bytes from the PipeWriter
+    //                Memory<byte> memory = writer.GetMemory(minimumBufferSize);
+
+    //                int bytesRead = await socket.ReceiveAsync(memory, SocketFlags.None);
+    //                if (bytesRead == 0)
+    //                {
+    //                    break;
+    //                }
+
+    //                // Tell the PipeWriter how much was read
+    //                writer.Advance(bytesRead);
+    //            }
+    //            catch
+    //            {
+    //                break;
+    //            }
+
+    //            // Make the data available to the PipeReader
+    //            FlushResult result = await writer.FlushAsync();
+
+    //            if (result.IsCompleted)
+    //            {
+    //                break;
+    //            }
+    //        }
+
+    //        // Signal to the reader that we're done writing
+    //        writer.Complete();
+    //    }
+
+    //    private static async Task ReadPipeAsync(Socket socket, PipeReader reader)
+    //    {
+    //        while (true)
+    //        {
+    //            ReadResult result = await reader.ReadAsync();
+
+    //            ReadOnlySequence<byte> buffer = result.Buffer;
+    //            SequencePosition? position = null;
+
+    //            do
+    //            {
+    //                // Find the EOL
+    //                position = buffer.PositionOf((byte)'\n');
+
+    //                if (position != null)
+    //                {
+    //                    var line = buffer.Slice(0, position.Value);
+    //                    ProcessLine(socket, line);
+
+    //                    // This is equivalent to position + 1
+    //                    var next = buffer.GetPosition(1, position.Value);
+
+    //                    // Skip what we've already processed including \n
+    //                    buffer = buffer.Slice(next);
+    //                }
+    //            }
+    //            while (position != null);
+
+    //            // We sliced the buffer until no more data could be processed
+    //            // Tell the PipeReader how much we consumed and how much we left to process
+    //            reader.AdvanceTo(buffer.Start, buffer.End);
+
+    //            if (result.IsCompleted)
+    //            {
+    //                break;
+    //            }
+    //        }
+
+    //        reader.Complete();
+    //    }
+
+    //    private static void ProcessLine(Socket socket, in ReadOnlySequence<byte> buffer)
+    //    {
+    //        Console.Write($"[{socket.RemoteEndPoint}]: ");
+    //        foreach (var segment in buffer)
+    //        {
+
+    //            Console.Write(Encoding.UTF8.GetString(segment.Span));
+    //        }
+    //        Console.WriteLine();
+    //    }
+    //}
+
+}
+
