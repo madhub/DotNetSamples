3513c6a04df27ccf4d17d9907bfc9a7507a9c9f2
madhub <b.madhu@outlook.com>
2021-02-16 10:55:17 +0530

    Update Program.cs
    added expando object sample

--- CShap8Features/Program.cs
+++ CShap8Features/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Dynamic;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
 
 
 
 namespace CShap8Features
 {
+    // C# dyanamic
+   static void ExpandoObjectDemo()
+   {
+            dynamic customer = new ExpandoObject();
+            customer.Id = 42;
+            customer.sb = new StringBuilder("a string builder");
+
+            //if we want to dynamically add properties to this expando object,
+            //we can convert it to a dictionary first and then call Add method.
+
+            var c = (IDictionary<string, object>)customer;
+            c.Add("stringPropertyName", "propertyValue");
+
+            foreach (var item in c)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+    }
     class Program
     {
         static void Main(string[] args)
